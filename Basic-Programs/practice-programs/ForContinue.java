c874f82c62c68ded2f38785615a41482ae4e836a
riddhish1011 <riddhi.sh1011@gmail.com>
2026-01-29 15:00:17 +0530

    added file

---
+++ Basic-Programs/practice-programs/ForContinue.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ForContinue{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter your number: ");
+int n=sc.nextInt();
+for (int i=1; i<=n; i++){
+if(i%2==0){
+continue;
+}
+else{
+System.out.println(i);
+}
+}
+}
+}
